57c9a2bfe127191c36b47e1b8decfd645428d9d5
CareyCunningham <cunningc@sheridancollege.com>
2023-02-21 10:50:17 -0500

    added Carey class

---
+++ src/midtermexam_versiona_extensioncode/Carey.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author Carey Cunningham
+ * date:   2023-02-21
+ * Branch for CareyCunninghaam to create class Carey
+ */
+public class Carey {
+    
+}
